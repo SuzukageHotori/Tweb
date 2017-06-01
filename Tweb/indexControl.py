a94eeb652f6a32fbb53b0b1aeea59524e8f9f6f5
SuzukageHotori <Taburiss@Taburiss.com>
2017-06-01 11:40:09 +0800

    submit

--- Tweb/indexControl.py
+++ Tweb/indexControl.py
@@ -5,9 +5,9 @@
 
 __author__ = 'Taburiss'
 
-from flask import Flask,request,render_template;
-import encodings
-app = Flask(__name__)
+from flask import Flask,request,render_template,url_for,jsonify
+#DEBUG = True
+app = Flask(__name__,static_url_path='/static', static_folder='static')
 
 
 class homeModel(object):
@@ -21,6 +21,7 @@ class homeModel(object):
 @app.route('/', methods=['GET', 'POST'])
 def home():
     model = homeModel();
+    #a = url_for("static",filename="img/timg.jpg") ;
     return  render_template('/home/Index.html',model=model);
 
 
